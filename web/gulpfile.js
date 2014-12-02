0cfe3084e2ec032f02f39ac640e3bc582a4d23e4
Sander Looijenga <sander.looijenga@hva.nl>
2014-12-02 21:50:09 +0100

    Updated the build process + changed name in admin template.

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,29 +1,27 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var gulp = require('gulp'),
+    rename = require("gulp-rename"),
+    concat = require("gulp-concat"),
+    uglify = require('gulp-uglify'),
+    watch = require("gulp-watch"),
+    livereload = require('gulp-livereload');
 
-
-var gulp = require('gulp');
-var rename = require("gulp-rename");
-var concat = require("gulp-concat");
-var uglify = require('gulp-uglify');
-var watch = require("gulp-watch");
-var livereload = require('gulp-livereload');
-        
 gulp.task("default", ["build", "watch"]);
 
 gulp.task("watch", function(){
-    gulp.watch('./js/lib/*.js', ['build'])
+    gulp.watch('./js/lib/*.js', ['build']);
 });
 
 gulp.task('build', function() {
-  gulp.src('./js/lib/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./js/dist/'))
-    .pipe(rename("main.min.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest('./js/dist/'));
+    gulp.src('./js/lib/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./js/dist/'))
+        .pipe(rename('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js/dist/'));
+    gulp.src('./js/admin/*.js')
+        .pipe(concat('admin.js'))
+        .pipe(gulp.dest('./js/dist/'))
+        .pipe(rename('admin.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js/dist/'));
 });
-
